7b18c12c88e02f931e80f0076bee7873cdfea979
rubynovich <roman@shipiev.me>
2013-03-19 10:23:49 +0400

    ops

--- app/models/meeting_room_reserve.rb
+++ app/models/meeting_room_reserve.rb
@@ -1,67 +1,67 @@
 class MeetingRoomReserve < ActiveRecord::Base
   unloadable
-  
+
   belongs_to :user
   belongs_to :meeting_room
-  
-  validates_presence_of :user_id, :meeting_room_id, :subject, 
+
+  validates_presence_of :user_id, :meeting_room_id, :subject,
     :reserve_on, :start_time, :end_time
   validates_associated :user, :meeting_room
   validates_uniqueness_of :reserve_on, :scope => [:start_time, :end_time, :meeting_room_id]
-    
+
   validate :check_start_time, :check_end_time, :check_reserve_on
 
-  if Rails::VERSION::MAJOR >= 3  
+  if Rails::VERSION::MAJOR >= 3
     scope :actual_day, lambda{
       date = Date.today
       time = Time.now
       where("reserve_on > :date OR reserve_on = :date AND end_time > :time", {:date => date, :time => time})
     }
-      
+
     scope :included, lambda{ |date, time|
       if date.present? && time.present?
         where("reserve_on = :date AND start_time < :time AND end_time > :time", {:date => date, :time => time})
-      end          
-    }  
-  
+      end
+    }
+
     scope :for_meeting_room, lambda{ |meeting_room_id|
       if meeting_room_id.present?
         where(:meeting_room_id => meeting_room_id)
       end
     }
-  else 
+  else
     named_scope :actual_day, lambda{
       date = Date.today
       time = Time.now
       {
         :conditions => [
           "reserve_on > :date OR reserve_on = :date AND end_time > :time", {
-            :date => date, 
+            :date => date,
             :time => time}
           ]
       }
     }
-      
+
     named_scope :included, lambda{ |date, time|
       if date.present? && time.present?
-        { :conditions => 
+        { :conditions =>
             ["reserve_on = :date AND start_time < :time AND end_time > :time",
               {
-                :date => date, 
+                :date => date,
                 :time => time
               }
             ]
         }
-      end          
-    }  
-  
+      end
+    }
+
     named_scope :for_meeting_room, lambda{ |meeting_room_id|
       if meeting_room_id.present?
         {:conditions => {:meeting_room_id => meeting_room_id}}
       end
     }
   end
-    
+
   def check_start_time
     if self.class.for_meeting_room(self.meeting_room_id).
       included(self.reserve_on, self.start_time).all.any? ||
@@ -70,20 +70,20 @@ class MeetingRoomReserve < ActiveRecord::Base
       errors.add :start_time, :invalid
     end
   end
-  
+
   def check_end_time
     if self.class.for_meeting_room(self.meeting_room_id).
       included(self.reserve_on, self.end_time).all.any? ||
       self.end_time.seconds_since_midnight > self.meeting_room.end_time.seconds_since_midnight ||
       self.end_time.seconds_since_midnight < self.start_time.seconds_since_midnight
-      
+
       errors.add :end_time, :invalid
-    end  
+    end
   end
-  
+
   def check_reserve_on
-    if self.reserve_on < Date.today
+    if self.reserve_on.blank? || self.reserve_on < Date.today
       errors.add :reserve_on, :invalid
-    end    
+    end
   end
 end
